c40c460d3703ec4085c9202891c25550e8877b62
danim64 <danim64@hotmail.com>
2022-06-02 21:49:04 -0500

    dan

--- GitTest.py
+++ GitTest.py
@@ -1,19 +1,7 @@
-
-option = (input ("Ingrese su nombre: "))
-
-if option == "Miguel Jimenez":
-    #Insert code here
-    print("Opción 1")
-
-if option == "":
-    #Insert code here
-    print("")
-
 option = (input ("Ingrese su nombre"))
 
-if option == "Virgilio Salgado":
+if option == "Deivid Guauña":
     #Insert code here
-    print("First Test")
     print("option")
 
 if option == "Jesus Gonzalez":
@@ -21,10 +9,3 @@ if option == "Jesus Gonzalez":
     print(name)
     print("El cambio en primera")
 
-if option == "cheider":
-    name = option.split()
-    print(name)
-    print("Bienvenido cheider")
-
-if option == "Yisel Saez":
-    print("Hola!!")
