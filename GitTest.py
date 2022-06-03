cd4dc05baa87e49cc8aceeefcffa857641dcc79e
aimerr <aimerr@uninorte.edu.co>
2022-06-02 21:29:54 -0500

    Commit Aimer R.

--- GitTest.py
+++ GitTest.py
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-option = input ("Insert a number between 1 and 40: ")
+option = input ("Ingrese su nombre:")
 
 if option == "Aimer R.":
     #Insert code here
     print("Hola : "+option)
-=======
-option = (input ("Ingrese su nombre"))
 
-if option == "Deivid Guauña":
-    #Insert code here
-<<<<<<< HEAD
-    print("El cambio en primera")
->>>>>>> 44967002028da6e7e715c9e88684aef88679b386
-=======
-    print("option")
-
-if option == "Jesus Gonzalez":
-    name = option.split()
-    print(name)
-    print("El cambio en primera")
->>>>>>> 5e00fa0b2bd6b4969b9dbc0cc3a6fb9aa26d2b75
