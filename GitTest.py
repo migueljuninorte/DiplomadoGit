5dcbfa7c046307e26432de3b9a8c91db0436bd2a
Jesus Gonzalez <gonzalezjesus29s@gmail.com>
2022-06-02 21:18:48 -0500

    cambio jesus gonzalez

--- GitTest.py
+++ GitTest.py
@@ -3,3 +3,7 @@ option = int(input ("Insert a number between 1 and 40: "))
 if option == 1:
     #Insert code here
     print("option")
+
+if option == "Jesus Gonzalez":
+    name = option.split()
+    print(name)
