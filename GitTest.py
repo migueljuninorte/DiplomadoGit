4d179c16600447d91e4e71470821a84b5f1a64af
aimerr <aimerr@uninorte.edu.co>
2022-06-04 21:37:50 -0500

    Prueba Commit

--- GitTest.py
+++ GitTest.py
@@ -1,29 +1,33 @@
 option = (input ("Ingrese su nombre: "))
 
-if option == "Miguel Jimenez":
-    #Insert code here
-    print("Opción 1")
 
 
-if option == "Angel Barros":
-    #Insert code here
-    print("")
 
-if option == "Deivid Guauña":
-    #Insert code here
-    print("option")
 
-if option == "Jesus Gonzalez":
-    name = option.split()
-    print(name)
-    print("El cambio en primera")
 
-if option == "cheider":
-    name = option.split()
-    print(name)
-    print("Bienvenido cheider")
 
-if option == "Yisel Saez":
-    print("Hola!!")
-if option == "Nicolas Lanete":
-    print("Hola2.0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if option == "Aimer Rodriguez":
+    print("Hola "+option)
